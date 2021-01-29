a2a3ace4c9d3ed5e8958f8cee421b09b6d46f869
Torsten Werner <torsten.werner@westernacher.com>
2021-01-29 22:47:43 +0100

    avoid warning about Serializable

--- src/main/java/xyz/itsme/uiplayground/Business.java
+++ src/main/java/xyz/itsme/uiplayground/Business.java
@@ -3,11 +3,12 @@ package xyz.itsme.uiplayground;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Scope("view")
 @Component
-public class Business {
+public class Business implements Serializable {
     private final AtomicInteger counter = new AtomicInteger(0);
 
     public int getCounter() {
